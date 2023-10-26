5e4d7631f3e20cd3a84ca50b415be0a27a2d7467
tiana-kpakpo <tianakpakpo1@gmail.com>
2023-10-26 22:14:45 +0000

    first

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './public/index.html'
+  ],
+  theme: {
+    container: {
+      center: true,
+      padding:" 2rem",
+
+    },
+  },
+  plugins: [],
+}
+
